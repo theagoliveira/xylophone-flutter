801220fd0b7222012b1f76f0351516b46e39a76a
Thiago Cavalcante <thiago.kun@gmail.com>
2021-08-12 17:47:25 -0300

    Update audioplayers plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
